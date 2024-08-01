773bb194ba272d831d81d2006fc141a053b8376d
FelipeAndresLopez <felipe.lopez@uptc.edu.co>
2024-08-01 09:27:56 -0500

    commit mappers

--- bank-service/src/main/java/com/felipelopez/bank_service/mapper/AccountMapper.java
+++ bank-service/src/main/java/com/felipelopez/bank_service/mapper/AccountMapper.java
@@ -1,4 +1,31 @@
 package com.felipelopez.bank_service.mapper;
 
+import com.felipelopez.bank_service.model.dto.AccountRequestDTO;
+import com.felipelopez.bank_service.model.entity.Account;
+import lombok.AllArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+@AllArgsConstructor
 public class AccountMapper {
+
+    private final ModelMapper modelMapper;
+
+    public Account convertToEntity(AccountRequestDTO accountRequestDTO) {
+        return modelMapper.map(accountRequestDTO, Account.class);
+    }
+
+    public AccountRequestDTO convertToDTO(Account account) {
+        return modelMapper.map(account, AccountRequestDTO.class);
+    }
+
+    public List<AccountRequestDTO> convertToDTO(List<Account> accounts) {
+
+        return accounts.stream()
+                .map(this::convertToDTO)
+                .toList();
+    }
 }
--- bank-service/src/main/java/com/felipelopez/bank_service/mapper/TransactionMapper.java
+++ bank-service/src/main/java/com/felipelopez/bank_service/mapper/TransactionMapper.java
@@ -1,4 +1,39 @@
 package com.felipelopez.bank_service.mapper;
 
+import com.felipelopez.bank_service.model.dto.TransactionReportDTO;
+import com.felipelopez.bank_service.model.dto.TransactionRequestDTO;
+import com.felipelopez.bank_service.model.dto.TransactionResponseDTO;
+import com.felipelopez.bank_service.model.entity.Transaction;
+import lombok.AllArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Component
+@AllArgsConstructor
 public class TransactionMapper {
+
+    private final ModelMapper modelMapper;
+
+    public Transaction convertToEntity(TransactionRequestDTO transactionDTO) {
+        return modelMapper.map(transactionDTO, Transaction.class);
+    }
+
+    public TransactionResponseDTO convertToDTO(Transaction transaction) {
+        return modelMapper.map(transaction, TransactionResponseDTO.class);
+    }
+
+    public List<TransactionResponseDTO> convertToListDTO(List<Transaction> transactions) {
+        return transactions.stream()
+                .map(this::convertToDTO)
+                .toList();
+    }
+    public TransactionReportDTO convertTransactionReportDTO(Object[] transactionData){
+        TransactionReportDTO reportDTO = new TransactionReportDTO();
+        reportDTO.setDate((LocalDate) transactionData[0]);
+        reportDTO.setTransactionCount((long) transactionData[1]);
+        return reportDTO;
+    }
 }
