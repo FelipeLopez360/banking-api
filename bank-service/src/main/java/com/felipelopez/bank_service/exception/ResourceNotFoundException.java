01cc073c9c361936314d6a7ba66e092cfc4940c5
FelipeAndresLopez <felipe.lopez@uptc.edu.co>
2024-08-05 22:30:02 -0500

    creación de manejo de excepciones.

--- bank-service/src/main/java/com/felipelopez/bank_service/exception/ResourceNotFoundException.java
+++ bank-service/src/main/java/com/felipelopez/bank_service/exception/ResourceNotFoundException.java
@@ -1,4 +1,10 @@
 package com.felipelopez.bank_service.exception;
 
-public class ResourceNotFoundException {
+public class ResourceNotFoundException extends RuntimeException{
+    public ResourceNotFoundException() {
+    }
+
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
 }
