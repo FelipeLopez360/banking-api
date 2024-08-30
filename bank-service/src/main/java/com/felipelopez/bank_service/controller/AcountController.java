806a6c880eb60271e7949fa290a2259d8ca41f4c
FelipeAndresLopez <felipe.lopez@uptc.edu.co>
2024-08-29 23:35:36 -0500

    implementa account controllers

--- bank-service/src/main/java/com/felipelopez/bank_service/controller/AcountController.java
+++ bank-service/src/main/java/com/felipelopez/bank_service/controller/AcountController.java
@@ -1,4 +1,50 @@
 package com.felipelopez.bank_service.controller;
 
+import com.felipelopez.bank_service.model.dto.AccountRequestDTO;
+import com.felipelopez.bank_service.model.dto.AccountResponseDTO;
+import com.felipelopez.bank_service.service.AccountService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/accounts")
+@AllArgsConstructor
 public class AcountController {
+
+    private final AccountService accountService;
+
+    @GetMapping
+    public ResponseEntity<List <AccountResponseDTO>> getAllAccounts(){
+        List<AccountResponseDTO> accounts = accountService.getAllAccounts();
+        return new ResponseEntity<>(accounts, HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<AccountResponseDTO> getAccountById(@PathVariable Long id){
+        AccountResponseDTO account = accountService.getAccountById(id);
+        return new ResponseEntity<>(account, HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<AccountResponseDTO> createAccount(@RequestBody AccountRequestDTO accountRequestDTO){
+        AccountResponseDTO createAccount = accountService.createAccount(accountRequestDTO);
+        return new ResponseEntity<>(createAccount, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<AccountResponseDTO> updateAccount(@PathVariable Long id, @RequestBody AccountRequestDTO accountRequestDTO){
+        AccountResponseDTO updateAccount = accountService.updateAccount(id, accountRequestDTO);
+        return new ResponseEntity<>(updateAccount, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteAccount(@PathVariable Long id){
+        accountService.deleteAccount(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
 }
